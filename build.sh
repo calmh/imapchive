9f991af35fc9c229a73eb63b145a1f3ac74881b6
Jakob Borg <jakob@kastelo.net>
2020-05-16 13:19:53 +0200

    Skip archiving in build script

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ go install -v -race -ldflags "-X main.version=$version"
 [[ ${1:-} == "pkg" ]] || exit 0
 
 build() {
-    name="imapchive-$version"
+    name="imapchive-$version-$1-$2"
     rm -rf "build/$name"
     mkdir -p "build/$name"
     GOOS=$1 GOARCH=$2 go build -i -v -ldflags "-s -w -X main.version=$version" -o "build/$name/imapchive$3"
@@ -17,15 +17,10 @@ build() {
 rm -rf build
 for os in darwin freebsd ; do
     build $os amd64 ""
-    tar -C build -zcf imapchive-$version-${os/darwin/macos}-amd64.tar.gz imapchive-$version
 done
 for arch in amd64 386 ; do
     build windows $arch ".exe"
-    pushd build
-    zip -q -r ../imapchive-$version-windows-$arch.zip imapchive-$version
-    popd
 done
 for arch in amd64 386 arm64 arm mips mipsle mips64 mips64le ; do
     build linux $arch ""
-    tar -C build -zcf imapchive-$version-linux-$arch.tar.gz imapchive-$version
 done
